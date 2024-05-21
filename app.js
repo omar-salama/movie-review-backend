47e3f5e97022f882334d38fcfdfc505d103fe9b1
Omar Salama <omarzalama@gmail.com>
2024-05-21 23:46:06 +0300

    edit mongoose connection

--- app.js
+++ app.js
@@ -15,10 +15,9 @@ app.use(bodyParser.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('Connected to MongoDB'));
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Middlewares
 app.use(morgan("common"));
